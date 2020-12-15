31b704d87e8a1e290df0e2a230661cd55d6c9ccf
NameerRehman <50919560+NameerRehman@users.noreply.github.com>
2020-12-15 16:43:57 -0500

    User input added

--- BOM_Spares.py
+++ BOM_Spares.py
@@ -7,11 +7,13 @@ Created on Tue Aug 18 16:08:28 2020
 
 import pandas as pd
 import glob, os
-from xlrd import open_workbook
 
-#Read all xls files in Modules folder
-path = "C:/Users/nrehman/Documents/Modules"
+#Read all xls files in user specified folder
+path = input("Enter path for module files: ") #Ex: C:/Users/nrehman/Documents/Modules
 all_modules = glob.glob(path + "/*.xls")
+
+outputfile_name = input('Enter name for output file: ')
+outputfile_path = path + "/" + outputfile_name + ".xlsx"
 
 #Append each file contents to dataframe
 bom = pd.concat((pd.read_excel(m, header=7).assign(MODULE=os.path.basename(m)) for m in all_modules))
@@ -31,7 +33,7 @@ bom['MODULE'] = bom['MODULE'].str.replace('.xls','')
 bom['SPARE CLASS'] = pd.Categorical(bom['SPARE CLASS'], ['1','2','3','9','X'])
 bom = bom.sort_values('SPARE CLASS')
 
-#Create new df without Space Class X items
+#Create new df without Space Class 'X' items
 bom_spares = bom[bom['SPARE CLASS'] != 'X']
 
 #Create new df without duplicates
@@ -39,13 +41,17 @@ bom_spares_unique = bom_spares.drop_duplicates(subset = ['PART NUMBER'])
 
 #Iterate through each unique part in df
 for i in range(bom_spares_unique.shape[0]):
+    #extract part number
     part = bom_spares_unique['PART NUMBER'].iloc[i]
     
     #filter bom_spares by "part" & output MODULE column to a list
+    #provides all module occurences of "part"
     modules_list = bom_spares[bom_spares['PART NUMBER'] == part]['MODULE'].to_list()
     
-    #assign all module occurences of "part" to bom_spares_unique MODULE column
+    #replace MODULE column in bom_spares_unique with modules_list
     bom_spares_unique['MODULE'].iloc[i] = modules_list
+    
+   ##TODO: GET SUM OF COLUMN INSTEAD to reduce O^n##
     
     #filter bom_spares by "part" & output PROJ QTY column to a list
     total_qty = bom_spares[bom_spares['PART NUMBER'] == part]['PROJ\nQTY.'].to_list()
@@ -57,9 +63,10 @@ for i in range(bom_spares_unique.shape[0]):
     bom_spares_unique['PROJ\nQTY.'].iloc[i] = total_qty_sum
 
 #Export df to excel sheet
-with pd.ExcelWriter('Spare Parts List.xlsx') as writer:
+with pd.ExcelWriter(outputfile_path) as writer:
     bom.to_excel(writer, sheet_name='All Parts')
     bom_spares.to_excel(writer, sheet_name='Spares')
     bom_spares_unique.to_excel(writer, sheet_name='Spares Unqiue')
 
+print("\n Spare Parts List Created, " + outputfile_path)
 
